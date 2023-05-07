59d89c7eb1e2917e099f6d16764218f5dbb873c2
yoyo <yannlefou@gmail.com>
2023-05-07 23:01:33 +0200

    style des streams

--- Ext_Info/js/stream.js
+++ Ext_Info/js/stream.js
@@ -45,51 +45,45 @@ document.getElementById('buResRem').addEventListener('click', function() {
 
 
 // Stream ***************************************************************************************************************************************************************
-var Streamer =  [{
-    "name": "ryulgc",
-    "user_id": 884412255,
-    "img": "https://static-cdn.jtvnw.net/user-default-pictures-uv/ebe4cd89-b4f4-4cd9-adac-2f30151b4209-profile_image-300x300.png"
-    },
-    {"name": "otplol_", "user_id": 622498423, "img": "https://static-cdn.jtvnw.net/jtv_user_pictures/787bd9dd-9367-45ed-a44d-d755427549b8-profile_image-70x70.png"},
-    {"name": "Kamet0", "user_id": 27115917, "img": "https://static-cdn.jtvnw.net/jtv_user_pictures/9e12862d-a8d0-4df2-81aa-e2241022dc98-profile_image-70x70.jpg"},
-    {"name": "Gotaga", "user_id": 24147592, "img": "https://static-cdn.jtvnw.net/jtv_user_pictures/69e324f6-fc7d-4131-89ed-227a955637cf-profile_image-70x70.png"},
-    {"name": "Chipsette_FR", "user_id": 101049128,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/1c008e6b-b840-4c57-9c2e-859a5b9a4c53-profile_image-70x70.png"},
-    {"name": "LCK", "user_id": 124425501,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/04b097ac-9a71-409e-b30e-570175b39caf-profile_image-70x70.png"},
-    {"name": "VALORANT","user_id": 490592527,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/ab49dc0a-4b33-4dba-adb7-2a0d7ed08f3a-profile_image-70x70.png"},
-    {"name": "RLEsport","user_id": 651809919,"img": "https://static-cdn.jtvnw.net/user-default-pictures-uv/ce57700a-def9-11e9-842d-784f43822e80-profile_image-70x70.png"},
-    {"name": "RocketBaguette","user_id": 139027213,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/6019724d-4158-4cc9-bd03-75d67960be48-profile_image-70x70.png"},
-    {"name": "jbzzed","user_id": 114497555,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/jbzzed-profile_image-3b2a4588b08af4b0-70x70.jpeg"},
-    {"name": "Rasmelthor","user_id": 97189013,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/1dee5831-e38c-4a4d-9af5-c04df4c1398d-profile_image-70x70.png"},
-    {"name": "AzleTV","user_id": 31832587,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/eb50ce67-e93f-4515-a4a9-9c7a27cda231-profile_image-70x70.png"},
-    {"name": "Pokemon","user_id": 36653045,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/dd000fb5-e41d-4676-a793-8d024030ac55-profile_image-70x70.png"},
-    {"name": "Skyyart","user_id": 70298660,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/f5287d16-d2c8-4a82-ad68-d621415d43f2-profile_image-300x300.png"},
-    {"name": "Mickalow","user_id": 30709418,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/1c7c4302-f5e3-49d1-9559-c9afbdcf19ac-profile_image-300x300.png"},
-    {"name": "FairyPeak","user_id": 78705692,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/c63b7e39-b766-4f4d-ad40-244c6f84bb14-profile_image-300x300.png"},
-    {"name": "Snayzy","user_id": 122048844,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/e196f442-50f7-4397-92e3-49b8e24295be-profile_image-300x300.jpeg"},
-    {"name": "PodaSaii","user_id": 127826699,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/d84fc4d7-ed83-4107-84bb-b98e26bce5e9-profile_image-300x300.png"},
-    {"name": "Nayson","user_id": 729371964,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/d8fa4420-d07a-40ff-9cfa-23d006405b05-profile_image-300x300.png"},
-    {"name": "Nikof","user_id": 110119637,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/6be7dd51-05b0-4e38-a8b8-44a29702bf1f-profile_image-300x300.png"},
-    {"name": "FuuRy_Off","user_id": 117323707,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/00dd2b3e-a986-4693-81ae-4ba9dbea414e-profile_image-300x300.png"},
-    {"name": "LPL","user_id": 124425627,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/8111888b-17e2-43b6-ac5f-10bb3451b130-profile_image-300x300.png"},
-    {"name": "RocketLeague","user_id": 57781936,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/4f182bee-012a-4955-9bd2-55d903f84276-profile_image-300x300.png"},
-    {"name": "Ninou","user_id": 446661471,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/0884db5e-c50e-4656-809a-034e42c01bd3-profile_image-300x300.png"},
-    {"name": "Vatira_","user_id": 493309537,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/b1288865-884c-40e9-ac88-0c34656580a4-profile_image-300x300.png"},
-    {"name": "Etoiles","user_id": 85800130,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/4eecaab8-4de3-4bcc-b2b7-f3a03e8273a8-profile_image-300x300.png"},
-    {"name": "Mawkzy_","user_id": 625931244,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/6caeccff-e661-4ccb-838f-a6426ac7e3a5-profile_image-300x300.png"},
-    {"name": "Kaydop","user_id": 63675549,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/047f3327-ef6d-4e0d-bd65-38a0586b195c-profile_image-300x300.png"},
-    {"name": "GaspowRL","user_id": 121767601,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/8877e8f2-fccf-4ccf-8b28-e2c353c1eb76-profile_image-300x300.png"},
-    {"name": "Akytio","user_id": 27621732,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/eeb41690-189a-40fe-a1b2-ebccf3795c21-profile_image-300x300.png"},
-    {"name": "Kyedae","user_id": 482881740,"img": "https://static-cdn.jtvnw.net/user-default-pictures-uv/998f01ae-def8-11e9-b95c-784f43822e80-profile_image-300x300.png"},
-    {"name": "D7RL","user_id": 171183040,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/c794340d-322e-41b4-86d7-61a8f15d6686-profile_image-300x300.png"},
-    {"name": "fugu_fps","user_id": 140846786,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/938e2765-7a70-4cdc-93ab-844b7b729e09-profile_image-300x300.png"},
-    {"name": "Solary","user_id": 174955366,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/3b6094ef-a538-42c9-ad59-25a0fe719ee2-profile_image-300x300.png"},
-    {"name": "LeBouseuh","user_id": 96562014,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/9a6e58e5-2da6-47e4-a4ce-eb91a0d1572d-profile_image-300x300.png"},
-    {"name": "Mastu","user_id": 63936838,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/64e280b4-d6a1-4819-922b-9d4f8c80a3c7-profile_image-300x300.png"},
-    {"name": "Squeezie","user_id": 52130765,"img": "https://static-cdn.jtvnw.net/jtv_user_pictures/df3e5800-b524-4c4e-87b0-8b3646d9185a-profile_image-300x300.png"}
-]
-
-
-
+var Streamer =  {
+    "ryulgc": "https://static-cdn.jtvnw.net/user-default-pictures-uv/ebe4cd89-b4f4-4cd9-adac-2f30151b4209-profile_image-300x300.png",
+    "otplol_": "https://static-cdn.jtvnw.net/jtv_user_pictures/787bd9dd-9367-45ed-a44d-d755427549b8-profile_image-70x70.png",
+    "Kamet0": "https://static-cdn.jtvnw.net/jtv_user_pictures/9e12862d-a8d0-4df2-81aa-e2241022dc98-profile_image-70x70.jpg",
+    "Gotaga": "https://static-cdn.jtvnw.net/jtv_user_pictures/69e324f6-fc7d-4131-89ed-227a955637cf-profile_image-70x70.png",
+    "Chipsette_FR": "https://static-cdn.jtvnw.net/jtv_user_pictures/1c008e6b-b840-4c57-9c2e-859a5b9a4c53-profile_image-70x70.png",
+    "LCK": "https://static-cdn.jtvnw.net/jtv_user_pictures/04b097ac-9a71-409e-b30e-570175b39caf-profile_image-70x70.png",
+    "VALORANT": "https://static-cdn.jtvnw.net/jtv_user_pictures/375c4db6-af2a-489e-842a-5e5b3ce287a2-profile_image-70x70.png",
+    "RLEsport": "https://static-cdn.jtvnw.net/user-default-pictures-uv/ce57700a-def9-11e9-842d-784f43822e80-profile_image-70x70.png",
+    "RocketBaguette": "https://static-cdn.jtvnw.net/jtv_user_pictures/6019724d-4158-4cc9-bd03-75d67960be48-profile_image-70x70.png",
+    "jbzzed": "https://static-cdn.jtvnw.net/jtv_user_pictures/jbzzed-profile_image-3b2a4588b08af4b0-70x70.jpeg",
+    "Rasmelthor": "https://static-cdn.jtvnw.net/jtv_user_pictures/1dee5831-e38c-4a4d-9af5-c04df4c1398d-profile_image-70x70.png",
+    "AzleTV": "https://static-cdn.jtvnw.net/jtv_user_pictures/eb50ce67-e93f-4515-a4a9-9c7a27cda231-profile_image-70x70.png",
+    "Pokemon": "https://static-cdn.jtvnw.net/jtv_user_pictures/dd000fb5-e41d-4676-a793-8d024030ac55-profile_image-70x70.png",
+    "Skyyart": "https://static-cdn.jtvnw.net/jtv_user_pictures/f5287d16-d2c8-4a82-ad68-d621415d43f2-profile_image-300x300.png",
+    "Mickalow": "https://static-cdn.jtvnw.net/jtv_user_pictures/1c7c4302-f5e3-49d1-9559-c9afbdcf19ac-profile_image-300x300.png",
+    "FairyPeak": "https://static-cdn.jtvnw.net/jtv_user_pictures/c63b7e39-b766-4f4d-ad40-244c6f84bb14-profile_image-300x300.png",
+    "Snayzy": "https://static-cdn.jtvnw.net/jtv_user_pictures/e196f442-50f7-4397-92e3-49b8e24295be-profile_image-300x300.jpeg",
+    "PodaSaii": "https://static-cdn.jtvnw.net/jtv_user_pictures/d84fc4d7-ed83-4107-84bb-b98e26bce5e9-profile_image-300x300.png",
+    "Nayson": "https://static-cdn.jtvnw.net/jtv_user_pictures/d8fa4420-d07a-40ff-9cfa-23d006405b05-profile_image-300x300.png",
+    "Nikof": "https://static-cdn.jtvnw.net/jtv_user_pictures/6be7dd51-05b0-4e38-a8b8-44a29702bf1f-profile_image-300x300.png",
+    "FuuRy_Off": "https://static-cdn.jtvnw.net/jtv_user_pictures/00dd2b3e-a986-4693-81ae-4ba9dbea414e-profile_image-300x300.png",
+    "LPL": "https://static-cdn.jtvnw.net/jtv_user_pictures/8111888b-17e2-43b6-ac5f-10bb3451b130-profile_image-300x300.png",
+    "RocketLeague": "https://static-cdn.jtvnw.net/jtv_user_pictures/4f182bee-012a-4955-9bd2-55d903f84276-profile_image-300x300.png",
+    "Ninou": "https://static-cdn.jtvnw.net/jtv_user_pictures/0884db5e-c50e-4656-809a-034e42c01bd3-profile_image-300x300.png",
+    "Vatira_": "https://static-cdn.jtvnw.net/jtv_user_pictures/b1288865-884c-40e9-ac88-0c34656580a4-profile_image-300x300.png",
+    "Etoiles": "https://static-cdn.jtvnw.net/jtv_user_pictures/4eecaab8-4de3-4bcc-b2b7-f3a03e8273a8-profile_image-300x300.png",
+    "Mawkzy_": "https://static-cdn.jtvnw.net/jtv_user_pictures/6caeccff-e661-4ccb-838f-a6426ac7e3a5-profile_image-300x300.png",
+    "Kaydop": "https://static-cdn.jtvnw.net/jtv_user_pictures/047f3327-ef6d-4e0d-bd65-38a0586b195c-profile_image-300x300.png",
+    "GaspowRL": "https://static-cdn.jtvnw.net/jtv_user_pictures/8877e8f2-fccf-4ccf-8b28-e2c353c1eb76-profile_image-300x300.png",
+    "Akytio": "https://static-cdn.jtvnw.net/jtv_user_pictures/eeb41690-189a-40fe-a1b2-ebccf3795c21-profile_image-300x300.png",
+    "Kyedae": "https://static-cdn.jtvnw.net/user-default-pictures-uv/998f01ae-def8-11e9-b95c-784f43822e80-profile_image-300x300.png",
+    "D7RL": "https://static-cdn.jtvnw.net/jtv_user_pictures/c794340d-322e-41b4-86d7-61a8f15d6686-profile_image-300x300.png",
+    "fugu_fps": "https://static-cdn.jtvnw.net/jtv_user_pictures/938e2765-7a70-4cdc-93ab-844b7b729e09-profile_image-300x300.png",
+    "Solary": "https://static-cdn.jtvnw.net/jtv_user_pictures/3b6094ef-a538-42c9-ad59-25a0fe719ee2-profile_image-300x300.png",
+    "LeBouseuh": "https://static-cdn.jtvnw.net/jtv_user_pictures/9a6e58e5-2da6-47e4-a4ce-eb91a0d1572d-profile_image-300x300.png",
+    "Mastu": "https://static-cdn.jtvnw.net/jtv_user_pictures/64e280b4-d6a1-4819-922b-9d4f8c80a3c7-profile_image-300x300.png",
+    "Squeezie": "https://static-cdn.jtvnw.net/jtv_user_pictures/df3e5800-b524-4c4e-87b0-8b3646d9185a-profile_image-70x70.png"
+}
 
 var clientId = '09v5b6vch1hsywuyw8j0d5gqxik09j';
 var token = 'u1336g3trrgsdwayhw33g5g3ik463f';
@@ -134,14 +128,14 @@ function displayDivStream() {
 function cb(json) {
     if (json.data.length) {
         var division = document.createElement('div');
-        division.style = "border-top:1px; border-top-style:solid; width:430px;text-align: center;";
+        division.style = "width:430px;text-align: center;background-color: rgba(215, 143, 248,0.5); margin-bottom:10px;border-radius: 10px;box-shadow: 0 4px 6px rgba(0, 0, 0, 0.5);";
 
         var premierLigne = document.createElement('div');
-        premierLigne.style = "display:flex; justify-content:center; height : 40px;text-align:center";
+        premierLigne.style = "display:flex; justify-content:center; height : 40px;text-align:center; padding-top:10px;";
 
         var thumbnail = document.createElement('img');
-        //thumbnail.src = Streamer[9].img;
-        //thumbnail.style = "margin-right : 5px;border-radius:50%; width:30px; height:30px;display:inline;"
+        thumbnail.src = Streamer[json.data[0].user_name];
+        thumbnail.style = "margin-right : 5px;border-radius:50%; width:30px; height:30px;display:inline;"
         // thumbnail.src = json2.Streamer[0].img;
         //thumbnail.style = "display: flex; radius:50%;";
 
@@ -152,13 +146,18 @@ function cb(json) {
         name.className = "linkStream";
         name.style = "display: inline;height: 10px; font-family: 'Dongle', sans-serif;font-size: 25px;font-weight: 400; margin-right:5px;";
 
+        var ligne = document.createElement('p');
+        ligne.innerHTML = "-----------------------";
+        ligne.style = "font-family: 'Dongle', sans-serif; margin:0px;";
+
         var titre = document.createElement('p');
         titre.innerHTML = json.data[0].title;
-        titre.style = "display: inline;font-family: 'Dongle', sans-serif;font-size: 25px;font-weight: 400;font-style: italic;";
+        titre.style = "display: inline;font-family: 'Dongle', sans-serif;font-size: 25px;font-weight: 400;font-style: italic; padding-bottom: 10px;padding-right:5px; padding-left:5px;";
 
-        //premierLigne.appendChild(thumbnail);
+        premierLigne.appendChild(thumbnail);
         premierLigne.appendChild(name);
         division.appendChild(premierLigne);
+        division.appendChild(ligne);
         division.appendChild(titre);
         divStream.appendChild(division);
     } else {}
@@ -179,28 +178,22 @@ function getStream(id) {
     fetchTwitchAPI(url);
 }
 
-function Stream() {
+function Stream(json) {
     divStream.innerHTML = "";
     console.log()
-    for (let i=0; i < Streamer.length; i++) {
-        getStream(Streamer[i].user_id);
+    for (let i=0; i < json.Streamer.length; i++) {
+        getStream(json.Streamer[i].user_id);
     }
+    
 }
 
-Stream();
-
-
-// fetch('../json/stream.json')
-// .then((response) => {
-//     return response.json();
-// })
-// .then((jsondata) => {
-//     //cb2(jsondata)
-//     //streamersId = jsondata;
-//     //console.log(jsondata)
-//     settruc(jsondata)
-//     Stream(jsondata);
-// });
+fetch('../json/stream.json')
+.then((response) => {
+    return response.json();
+})
+.then((jsondata) => {
+    Stream(jsondata);
+});
 
 // $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
