2dc57ec35c14bc8caa5aa6b1e18cd8131215774a
truc <ya.fourni@icloud.com>
2023-06-07 12:06:54 +0200

    delete node module

--- Ext_Info/js/background.js
+++
@@ -1,4 +0,0 @@
-localStorage.setItem("testLocalStorage", JSON.stringify({test: "sa fonctionne"}))
-localStorage.setItem("testLocalStorage", JSON.stringify({test: "sa fonctionne tu est sur ?"}))
-chrome.alarms.create({ periodInDay: 1});
-chrome.alarms.onAlarm.addListener(() => {console.log("Alarm Works !!!!!!!");});
